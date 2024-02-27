fbabd9c69ed9fc4e7c99dd44c998d9be30d32bce
ashetty386 <ashetty386@gmail.com>
2024-02-27 23:07:00 +0530

    insertion and accessing of elements in array

--- array.py
+++ array.py
@@ -1,4 +1,5 @@
 #this is the practice of arrays
+#creation of arrays
 import array
 arr=array.array("i",[1,2,3,4,6])
 print("the new created array is")
@@ -8,3 +9,13 @@ d=array.array("d",[1.1,2.3,6.9])
 print("\nthe new created array is")
 for j in range(0,3):
     print(d[j], end=" ")
+#insertion of elements into arrays
+arr.insert(3,90)
+print("\narray after insertion is: ", end=" ")
+for i in range(0,len(arr)):
+    print(arr[i],end=" ")
+print()
+#accessing elements of array
+print("the element at 0th index is:",arr[0])
+print("the element at 1st index is", arr[1])
+print("the element at 2nd index is",arr[2])
